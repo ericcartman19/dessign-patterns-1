1763d2bf3ffe1c92609c4a8a97446d5196dcd794
beny <jlbeny100@gmail.com>
2018-04-09 00:39:15 +0200

    clean code & no longer needed to maintain array debido a que es la Factory class quien tiene una cache (un HashMap)

--- src/com/valoyes/patterns/flyweight/PaintApp.java
+++ src/com/valoyes/patterns/flyweight/PaintApp.java
@@ -3,30 +3,6 @@ package com.valoyes.patterns.flyweight;
 public class PaintApp {
 
 	public void render(int numberOfShappes) {
-
-		Shape[] shapes = new Shape[numberOfShappes + 1];
-
-		// este codigo guarro ya no sera necesario
-//		for (int i = 1; i < numberOfShappes; i++) {
-//			if (i % 2 == 0) {
-//
-//				shapes[i] = new Circle();
-//
-//				((Circle) shapes[i]).setRadius(i);
-//				((Circle) shapes[i]).setLineColor("red");
-//				((Circle) shapes[i]).setFillColor("white");
-//				shapes[i].draw();
-//				
-//			} else {
-//
-//				shapes[i] = new Rectangle();
-//
-//				((Rectangle) shapes[i]).setLength(i);
-//				((Rectangle) shapes[i]).setBreadth(i+i);
-//				((Rectangle) shapes[i]).setFillStyle("dotted");
-//				shapes[i].draw();
-//			}
-//		}
 		
 		Shape shape = null;
 		for(int i = 1; i <= numberOfShappes; i++) {
